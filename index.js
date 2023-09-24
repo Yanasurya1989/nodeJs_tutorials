f4b57e3965539b3b6e87a685ef103b237d8ef135
YanaWebProgrammer <teukita15@gmail.com>
2023-09-25 00:10:26 +0700

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+// const cetakNama = (nama) => {
+//     return `hi, nama saya ${nama}`
+// }
+
+// bentuk template literal code diatas
+// menggunakan parameter
+// const cetakNama = (nama) => `hi, nama saya ${nama}`
+
+// console.log(cetakNama('Yana'))
+
+// menggunakan variable
+require('./coba')
+
+// require('../script')
+const nama = 'Yana'
+const cetakNama = (nama) => `hi, nama saya ${nama}`
+console.log(cetakNama('Yana'))
---
+++ test/script.js
@@ -0,0 +1,9 @@
+// console.log(window.alert('hallo world'))
+
+function cetakNama(nama){
+    return `halo saya ${nama} dari directori berbeda`
+}
+
+require ('../index')
+
+console.log(cetakNama('belum bernama'))
